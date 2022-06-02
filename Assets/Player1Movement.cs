5af9315add9e65dbc47d9ad37f395ce427a623ce
ValentinaHsu <92767924+ValentinaHsu@users.noreply.github.com>
2022-06-02 14:41:45 -0300

    Initial commit

---
+++ Assets/Player1Movement.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    public float movementSpeed, rotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+
+    }
+}
